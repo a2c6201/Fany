0d51797faaceead7aa6468f9b628da97073224e5
a2c6201 <a2c6201@gmail.com>
2022-12-28 01:03:08 +0900

    APIを叩いて現在地付近のお店を取得

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from flask import Flask, render_template, request, url_for
 
@@ -14,7 +16,26 @@ def current_location():
   return data['latitude'], data['longitude']
 
 lat, lng = current_location()
-print(lat, lng)
+
+# 検索クエリ
+query = {
+        'key': hot_key, # APIキー
+        'lat': lat, # 現在地の緯度
+        'lng': lng, # 現在地の経度
+        'range': '4', # 2000m以内
+        'count': 50, # 取得データ数
+        'format': 'json' # データ形式json
+        }
+
+# URLとクエリでリクエスト
+responce = requests.get(hot_url, query)
+
+# 戻り値をjson形式で読み出し、['results']['shop']を抽出
+result = json.loads(responce.text)['results']['shop']
+
+# 店名、住所を表示
+for i in result:
+    print(i['name']+' : '+i['address'])
 
 app = Flask(__name__)
 
