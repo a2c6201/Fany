51553c5e1974328a69e42e28d62387b5293ca1a3
a2c6201 <a2c6201@gmail.com>
2023-01-02 01:59:44 +0900

    実験：コメントアウト

--- config.py
+++ config.py
@@ -18,15 +18,15 @@ HOT_PEPPER_API_URL = os.environ['HOT_PEPPER_API_URL']
 GEOCODING_API_URL = os.environ['GEOCODING_API_URL']
 GOOGLE_MAPS_API_URL = os.environ['GOOGLE_MAPS_API_URL']
 
-credential = DefaultAzureCredential()
-key_vault_url = os.environ["KEY_VAULT_URL"]
-keyvault_client = SecretClient(vault_url=key_vault_url, credential=credential)
-api_secret_name = os.environ["THIRD_PARTY_API_SECRET_NAME"]
-vault_secret = keyvault_client.get_secret(api_secret_name)
+# credential = DefaultAzureCredential()
+# key_vault_url = os.environ["KEY_VAULT_URL"]
+# keyvault_client = SecretClient(vault_url=key_vault_url, credential=credential)
+# api_secret_name = os.environ["THIRD_PARTY_API_SECRET_NAME"]
+# vault_secret = keyvault_client.get_secret(api_secret_name)
 
-# The "secret" from Key Vault is an object with multiple properties. The key we
-# want for the third-party API is in the value property.
-access_key = vault_secret.value
+# # The "secret" from Key Vault is an object with multiple properties. The key we
+# # want for the third-party API is in the value property.
+# access_key = vault_secret.value
 
-queue_url = os.environ["STORAGE_QUEUE_URL"]
-queue_client = QueueClient.from_queue_url(queue_url=queue_url, credential=credential)
+# queue_url = os.environ["STORAGE_QUEUE_URL"]
+# queue_client = QueueClient.from_queue_url(queue_url=queue_url, credential=credential)
