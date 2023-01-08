818f8f40cd150720b6f430cae3a946e9e3a98eb0
a2c6201 <a2c6201@gmail.com>
2023-01-08 16:20:22 +0900

    データベースに画像だけ追加可能に

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
+import base64
 import json
+import sqlite3
+from datetime import datetime
 
-from flask import Flask, render_template, request, session
+import pytz
+from flask import (Flask, flash, redirect, render_template, request, session,
+                   url_for)
 from flask_paginate import Pagination, get_page_parameter
+from flask_sqlalchemy import SQLAlchemy
 
 import config
 import fetcher as fe
 
+UPLOAD_FOLDER = './static'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
-app.secret_key = "range"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///photo.db'
+db = SQLAlchemy(app)
+app.secret_key = "range"  # sessionを暗号化
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 HOT_PEPPER_API_KEY = config.HOT_PEPPER_API_KEY
 HOT_PEPPER_URL = config.HOT_PEPPER_URL
 GOOGLE_MAPS_API_URL = config.GOOGLE_MAPS_API_URL
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    shop_id = db.Column(db.Integer, unique=True)
+    title = db.Column(db.Text(50), nullable=False)
+    image = db.Column(db.Text(500), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
 
 
 def shops_json(range, lat, lng):
@@ -43,15 +69,15 @@ def shop_json(id):
     return result[0]
 
 
-# 検索画面
 @app.route('/')
 def index():
+    # 検索画面
     return render_template('index.html')
 
 
-# 検索結果一覧
 @app.route('/result', methods=['GET', 'POST'])
 def result():
+    # 検索結果一覧
     if request.method == 'POST':
         if (request.headers['Content-Type'] == 'application/json'):
             session.permanent = True
@@ -88,9 +114,9 @@ def result():
     return render_template('result.html', shops=res, pagination=pagination)
 
 
-# 詳細画面
 @app.route('/detail/<shop_id>')
 def detail(shop_id):
+    # 詳細画面
     shop = shop_json(shop_id)
     lat = session['lat']
     lng = session['lng']
@@ -99,6 +125,32 @@ def detail(shop_id):
     )
     shop_url = HOT_PEPPER_URL.format(shop_id)
     return render_template('detail.html', shop=shop, map_url=map_url, shop_url=shop_url)
+
+
+@app.route('/create/<shop_id>', methods=['GET', 'POST'])
+def create(shop_id):
+    if request.method == "POST":
+    #     title = request.form.get('title')
+    #     image = request.files['image']
+    #     photo = Photo(shop_id=shop_id, title=title, image=image)
+    #     db.session.add(photo)
+    #     db.session.commit()
+    #     return redirect('/detail/{}'.format(shop_id))
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            img_base64 = base64.b64encode(file.read())
+            print(img_base64)
+            return redirect(url_for('detail', shop_id=shop_id))
+    return render_template('create.html')
 
 
 if __name__ == "__main__":
